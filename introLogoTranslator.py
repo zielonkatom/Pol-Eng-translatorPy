63e3d1938ab7ea10762b6691f861ce7c3c790d14
zielonkatom <zielonkatom@gmail.com>
2019-07-14 11:21:52 +0200

    Add files via upload

---
+++ introLogoTranslator.py
@@ -0,0 +1,13 @@
+
+def logo():
+    
+    print("".center(60, "."))
+    print("WELCOME".center(60, "."))
+
+
+    print("THIS IS A SIMPLE TRANSLATOR".center(60, "."))
+
+
+    print("JUST INPUT THE WORD(S)".center(60, "."))
+
+    print("".center(60, "."))
